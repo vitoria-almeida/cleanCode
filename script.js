3a6e9913eb4bda055863a05344a4823cd7f9cc62
Vitoria-m <vitoriazzz@outlook.com>
2022-09-21 16:59:46 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// A proporção de leitura e escrita de código é de 10 para 1. Ou seja, passamos muito mais tempo lendo códigos do que codando em si. Sendo assim, é essencial que escrevamos "códigos limpos", que sejam de fácil leitura, interpretação e manutenção.
+// 
+
+// 1 - 
+//blabla
+// codigo
